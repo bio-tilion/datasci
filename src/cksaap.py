99696a1cd268dd54d3ab75d8cecb49f99c49d6cc
bio-tilion <alberto.locca1@gmail.com>
2023-04-04 17:57:27 +0100

    added plotting function with test

--- src/cksaap.py
+++ src/cksaap.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 def cksaap(seq: str, k=2) -> np.ndarray:
@@ -12,7 +13,7 @@ def cksaap(seq: str, k=2) -> np.ndarray:
         k       Integer, number of spaces between the pair of amino acids
                 (Default 2)
 
-    Return a Numpy array of the encoded sequence
+    Returns a Numpy array of the encoded sequence
     """
     # get all possible permutations of pairs of amino acids
     aminoacids = "ACDEFGHIKLMNPQRSTVWY"
@@ -40,7 +41,33 @@ def cksaap(seq: str, k=2) -> np.ndarray:
 
     return seq_encoded
 
+def plot_encoding(seq_enc: np.ndarray):
+    """
+    Visualise encoding by plotting a heatmap of the amino acids pairs frequency
+
+    Arguments
+        seq_enc     Numpy array representing the encoded sequence
+
+    Returns a matplotlib.pyplot figure object
+    """
+    aa = "ACDEFGHIKLMNPQRSTVWY"
+    fig, ax = plt.subplots()
+    im = ax.imshow(seq_enc.reshape((20, 20)), cmap="cividis")
+    #cbar = plt.colorbar(im)
+
+    ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+    ax.set_xticks(np.arange(20), labels=list(aa))
+    ax.set_yticks(np.arange(20), labels=list(aa))
+
+    ax.spines[:].set_visible(False)
+    plt.title("Amino acids pairs frequency")
+    plt.colorbar(im)
+    plt.tight_layout()
+    return ax
+
 
 if __name__ == '__main__':
     #print(cksaap("ATAKAFCGAP", k=1))
+    #plot_encoding(cksaap("ATAKAFCGAP", k=0))
     help(cksaap)
+    help(plot_encoding)
--- test/test_cksaap.py
+++ test/test_cksaap.py
@@ -1,4 +1,8 @@
 from src.cksaap import cksaap
+from src.cksaap import plot_encoding
+import numpy as np
+import matplotlib.pyplot as plt
+import random
 
 def test_cksaap():
     """
@@ -16,3 +20,55 @@ def test_cksaap():
         # encoded array is a raveled 20x20 matrix
         # in this case the diagonal has to be 1
         assert seq_enc[i + (20 * i)] == 1
+
+def test_plot_encoding():
+    """
+    Test plot_encoding function
+        From a peptide sequence made of a single amino acid, compare the image got with the
+        plot_encoding function with the image got with the expected frequency matrix, by
+        importing the two images as array and comparing them
+    :return:
+    """
+
+    aa = "ACDEFGHIKLMNPQRSTVWY"
+
+    # get a random aa index
+    n = random.randint(0, 19)
+
+    # random length peptide made with a single amino acid
+    seq = aa[n] * 10
+
+    # encoding
+    seq_enc = cksaap(seq, k=1)
+
+    # get plot
+    fig = plot_encoding(seq_enc)
+    # convert image to a numpy array
+    canvas = fig.figure.canvas
+    canvas.draw()
+    image_plot = np.frombuffer(canvas.tostring_rgb(), dtype=np.uint8)
+    plt.close()
+
+    # get expected plot
+    # create frequency matrix
+    expected_enc = np.zeros((20, 20))
+    expected_enc[n][n] = 1
+    # same settings as plot_encoding function
+    fig, ax = plt.subplots()
+    im = ax.imshow(expected_enc, cmap="cividis")
+    ax.tick_params(top=True, bottom=False, labeltop=True, labelbottom=False)
+    ax.set_xticks(np.arange(20), labels=list(aa))
+    ax.set_yticks(np.arange(20), labels=list(aa))
+    ax.spines[:].set_visible(False)
+    plt.title("Amino acids pairs frequency")
+    plt.colorbar(im)
+    plt.tight_layout()
+
+    # convert image to a numpy array
+    canvas = ax.figure.canvas
+    canvas.draw()
+    image_expected = np.frombuffer(canvas.tostring_rgb(), dtype=np.uint8)
+    plt.close()
+
+    # compare the two image arrays
+    assert np.array_equal(image_plot, image_expected)
